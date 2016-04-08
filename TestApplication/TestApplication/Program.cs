339df19e7e797b5be1577aac46d7adeeb6a95adb
DESIGN\omor <aleks_morozova91@mail.ru>
2016-04-08 16:18:13 +0300

    Voloshyn commit

---
+++ TestApplication/TestApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Test_1, Test_2, my branch Nikita, Oleg branch, Voloshyn branch");
+            Console.ReadKey();
+        }
+    }
+}
