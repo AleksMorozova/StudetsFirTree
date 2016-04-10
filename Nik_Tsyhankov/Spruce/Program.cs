55ab86af59c59f07761c47a1820e95d89849d6fd
Nikita Tsyhankov <nikita1996.tsyhankov@gmail.com>
2016-04-10 23:15:13 +0300

    Final without tests

---
+++ Nik_Tsyhankov/Spruce/Program.cs
@@ -0,0 +1,25 @@
+﻿using Spruce.Props;
+using System;
+
+namespace Spruce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest _forest = new Forest();
+            Spruce _fir = new Spruce();
+
+            _forest.Trees.Add(_fir);
+            _forest.Trees[_forest.Trees.IndexOf(_fir)].ToSprout();
+            _forest.Trees[_forest.Trees.IndexOf(_fir)].Grow();
+
+            Year.ChangeSeason();
+            Year.ChangeSeason();
+            Year.ChangeSeason();
+            Year.ChangeSeason();
+
+            Console.ReadKey();
+        }
+    }
+}
