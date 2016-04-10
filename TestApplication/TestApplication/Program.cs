00aaeca85140bdf9049b2adb1e90b0bd8c74b521
valerabad <valerabaditsa@gmail.com>
2016-04-11 00:53:57 +0300

    deleted string

--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -9,8 +9,7 @@ namespace TestApplication
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine(" create branch BADITSA  Hello from Test_1, one big choke time from Test_1, Test_2 ");
+        {            
             Console.ReadKey();
         }
     }
