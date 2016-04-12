5884da9b732979023ca439c0a3efdfd0b7eeb685
valerabad <valerabaditsa@gmail.com>
2016-04-12 16:42:36 +0300

    new fix

--- Valeriy Baditsa/OOPSong/Fir.cs
+++ Valeriy Baditsa/OOPSong/Fir.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOPSong
 {
@@ -18,7 +14,7 @@ namespace OOPSong
             {
                 Born(this, EventArgs.Empty);
                 this.High = 0;
-                 this.Color= Colors.green;        
+                this.Color= Colors.green;        
             }
         }       
 
