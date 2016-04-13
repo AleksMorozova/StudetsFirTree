f371ade299a4f5165d7522bbe293cf7dcb1449e1
FedorVoloshyn <tikey.gm@gmail.com>
2016-04-13 17:29:59 +0300

    Залил финальную версию клуба плюс Елочку

---
+++ FedorVoloshyn/ISD_Course_Fight_club/ISD_Course_Fight_club/GameProcess.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISD_Course_Fight_club
+{
+    [Serializable]
+    class GameProcess : INotifyPropertyChanged
+    {
+        private Player user;
+        private Player computer;
+        private Player defender;
+        private string status;
+        private List<string> log;
+
+        [field: NonSerialized]
+        public event PlayerEvents FightOver;
+
+        public int UserHP { get { return user.Hp; } }
+        public string Status { get { return status; } }
+        public int ComputerHP { get { return computer.Hp; } }
+        public string Defender { get { return defender.Name; } }
+        public List<string> Log { get { return log; } }
+
+        public GameProcess()
+        {
+            user = new Player("User");
+            computer = new Player("Computer");
+            defender = computer;
+
+            user.Wound += WriteToLogWounded;
+            user.Blocked += WriteToLogBlocked;
+            user.Death += WriteToLogDeath;
+            computer.Wound += WriteToLogWounded;
+            computer.Blocked += WriteToLogBlocked;
+            computer.Death += WriteToLogDeath;
+
+            log = new List<string>();
+            log.Add("Fight!");
+        }
+        private void ChangeAttacker()
+        {
+            if (defender == user)
+            {
+                defender = computer;
+            }
+            else
+            {
+                defender = user;
+            }
+            SetStatus();
+        }
+        private void SetStatus()
+        {
+            if (ComputerHP <= 0 || UserHP <= 0)
+            {
+                status = "Fight over!";
+            }
+            else
+            {
+                if (defender == user)
+                {
+                    status = "Protect!";
+                }
+                else
+                {
+                    status = "Attack!";
+                }
+            }
+        }
+        public void Round(BodyPart choosenBodyPart)
+        {
+            if (defender == user)
+            {
+                defender.SetBlock(choosenBodyPart);
+                defender.GetHit(defender.GetRandomBodyPart());
+            }
+            else
+            {
+                defender.SetBlock(defender.GetRandomBodyPart());
+                defender.GetHit(choosenBodyPart);
+            }
+            OnPropertyChanged("Round");
+            ChangeAttacker();
+        }
+
+        private void WriteToLogWounded(string name, int health)
+        {
+            string roundResult = name + " wounded! HP: " + health.ToString();
+            log.Add(roundResult);
+        }
+        private void WriteToLogDeath(string name, int health)
+        {
+            string roundResult = name + " is dead!";
+            log.Add(roundResult);
+            if (FightOver != null)
+                FightOver(name, health);
+        }
+        private void WriteToLogBlocked(string name, int health)
+        {
+            string roundResult = name + " blocked punch! HP: " + health.ToString();
+            log.Add(roundResult);
+        }
+        public void Reset()
+        {
+            user.ResetHealth();
+            computer.ResetHealth();
+            defender = computer;
+            log.Clear();
+            log.Add("Fight!");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ FedorVoloshyn/ISD_Course_Fight_club/ISD_Course_Fight_club/Player.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISD_Course_Fight_club
+{
+    enum BodyPart
+    {
+        Head,
+        Body,
+        Legs
+    }
+        
+    public delegate void PlayerEvents(string name, int hp);
+    
+    [Serializable]
+    class Player
+    {
+        protected string name;
+        protected BodyPart blockedPart;
+        protected int hp = 100;
+        public string Name { get { return name; } }
+        public int Hp { get { return hp; } }
+
+        public event PlayerEvents Blocked;
+        public event PlayerEvents Wound;
+        public event PlayerEvents Death;
+
+        public Player(string Name)
+        {
+            this.name = Name;
+            hp = 100;
+        }
+
+        public void GetHit(BodyPart bodyPartToPunch)
+        {
+            if (bodyPartToPunch != blockedPart)
+            {
+                hp -= 20;
+                if (hp > 0)
+                {
+                    if (Wound != null)
+                        Wound(this.name, this.hp);
+                }
+                else
+                {
+                    if (Death != null)
+                        Death(this.name, this.hp);
+                }
+            }
+            else
+            {
+                if (Blocked != null)
+                    Blocked(this.name, this.hp);
+            }
+        }
+        public void SetBlock(BodyPart blockedPart)
+        {
+            this.blockedPart = blockedPart;
+        }
+
+        public void ResetHealth()
+        {
+            this.hp = 100;
+        }
+
+        public BodyPart GetRandomBodyPart()
+        {
+            Random rand = new Random();
+            BodyPart bodyPart = BodyPart.Head;
+
+            switch (rand.Next(0, 3))
+            {
+                case 0: bodyPart = BodyPart.Body;
+                    break;
+                case 1: bodyPart = BodyPart.Head;
+                    break;
+                case 2: bodyPart = BodyPart.Legs;
+                    break;
+            }
+            return bodyPart;
+        }
+    }
+    
+}
