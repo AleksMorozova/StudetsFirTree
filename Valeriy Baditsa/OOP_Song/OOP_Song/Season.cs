376bc01be51363c6b82d6d92ca28e53287d0da37
valerabad <valerabaditsa@gmail.com>
2016-04-16 00:29:02 +0300

    Added tests, refactoring, changed subscribed method,

---
+++ Valeriy Baditsa/OOP_Song/OOP_Song/Season.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP_Song
+{   
+    public class Season
+    {
+        public Seasons CurrentSeazon { get; set; }
+        double Temperature {get; set; }
+
+        public Season(Seasons currentSeason)
+        {
+            this.CurrentSeazon = currentSeason;
+            Console.WriteLine("Setted sezon = {0}", this.CurrentSeazon);
+        }
+
+        public event EventHandler<SeasonEventArgs> SeasonChanged;
+
+        public void OnSezonChanged(Seasons changedSeasonName)
+        {
+            CurrentSeazon = changedSeasonName;
+            Console.WriteLine("Season is changed");
+            if (SeasonChanged != null)
+            {
+                SeasonChanged(this, new SeasonEventArgs(changedSeasonName));                
+            }
+        }
+    }
+}
