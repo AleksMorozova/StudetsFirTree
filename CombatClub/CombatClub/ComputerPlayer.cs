f589360d4b054d5778eac1b2814000b158b2161e
valerabad <valerabaditsa@gmail.com>
2016-04-18 00:03:20 +0300

    Added CombatClub

---
+++ CombatClub/CombatClub/ComputerPlayer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatClub
+{
+    [Serializable]
+    class  ComputerPlayer : Player
+    {        
+        private Random rndm = new Random();
+        private int numberBodyPart;
+
+        public ComputerPlayer(Player loadPlayer)
+            : base(loadPlayer)
+        { }
+
+        public ComputerPlayer(string name, int hp) 
+             : base (name, hp)
+        {            
+                        
+        }
+        
+        override public void GetHit(BodyParts bodyPart)             
+        {
+            bodyPart = RandomBodyPart();
+            base.GetHit(bodyPart);                        
+        }
+        public BodyParts testc()
+        {
+            return BodyParts.body;
+        }
+
+        public override BodyParts ReturnAttackPartBody()
+        {
+            this.Attacked = RandomBodyPart();
+            return this.Attacked;
+        }
+
+        public BodyParts RandomBodyPart()
+        {
+            numberBodyPart = rndm.Next(3);
+            Attacked = (BodyParts)numberBodyPart;
+            return Attacked;
+        }
+
+        // рандомный выбор защищаемой части тела
+        public override void SetBlock(BodyParts bodyPart)
+        {
+            bodyPart = RandomBodyPart();
+            base.SetBlock(bodyPart);            
+        }
+    }
+}
